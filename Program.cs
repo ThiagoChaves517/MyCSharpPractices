bfc7b4531a965c68eaf7cf469ba2c7c625a07f87
Thiago Chaves <zuruarque@hotmail.com>
2022-07-24 17:51:45 -0400

    First Program in C#

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+void introYourSelf(string salutation = "Hello", string name = "???", string knownAs = "???")
+{
+    Console.WriteLine(salutation + "!");
+    Console.WriteLine("I'm " + name + ", " + knownAs + '.');
+}
+
+
+introYourSelf("Wassup", "Mikethelink", "the famous Xerê");
